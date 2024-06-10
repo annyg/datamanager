4a9d5ef6b1effde51858d9bfc19c7f7d95dab0dd
Anders Nygaard <anders.b.nygaard@gmail.com>
2024-06-10 16:33:59 +0200

    roxy to rand and email

--- R/prep_email.R
+++ R/prep_email.R
@@ -1,14 +1,29 @@
-#' Load unprocessed followup submissions
-#' @param x Messy email.
-#' @return Clean email.
+#' Preprocess email addresses for cleaning
+#'
+#' This function preprocesses email addresses for cleaning by replacing common mistakes with correct domain names and removing extra spaces.
+#'
+#' @param x Character vector containing email addresses.
+#' @return A character vector with preprocessed email addresses.
+#' @examples
+#' prep_email("example@email.com")
 #' @export
+#'
 prep_email <- function(x) {
-  #library(stringr)
+  library(stringr)
   email <- str_to_lower(
     str_replace_all(
-      str_squish(x), c(" "="",
-                         "\\("="",
-                         "\\)"="")
+      str_squish(x),
+      c(" "="",
+        "\\("="",
+        "\\)"="",
+        "gamil"="gmail",
+        "gamil"="gmail",
+        "gmal"="gmail",
+        "hotnail"="hotmail",
+        "yaoo"="yahoo",
+        "yahou"="yahoo",
+        "yaho"="yahoo")
     ))
-  #print(email)
+  print(email)
 }
+
--- R/scoring_rand12_v2.R
+++ R/scoring_rand12_v2.R
@@ -1,27 +1,44 @@
-# Made according to
-# Andersen, J. R., et al. (2022). Correlated physical and mental health composite
-# scores for the RAND-36 and RAND-12 health surveys: Can we keep them simple? Health
-# and Quality of Life Outcomes, 20(1), 89. https://doi.org/10.1186/s12955-022-01992-0
-
-# This function generates the unweighted RAND-12 PCS and MCS scores that range from 0 to 100, with higher scores indicating better HRQoL.
-
-scoring_rand12_unweighted <- function(df,
-                                RAND36_1 = Health_rating_now, #RAND12_1,
-                                RAND36_3b = Moderate_activity4w, #RAND12_2a,
-                                RAND36_3d = Stairs4w, #RAND12_2b,
-                                RAND36_4b = Physical_health_lessthanwanted4w, #RAND12_3a,
-                                RAND36_4c = Physical_health_limited4w, #RAND12_3b,
-                                RAND36_5b = Psychological_health_lessthanwanted4w, #RAND12_4a,
-                                RAND36_5c = Psychological_healthlimitation4w, #RAND12_4b,
-                                RAND36_8 = Pain_limited_activities4w, #RAND12_5,
-                                RAND36_9d = Relaxed_feeling4w, #RAND12_6a,
-                                RAND36_9e = Energetic_feeling4w, #RAND12_6b,
-                                RAND36_9f = Depressed_feeling4w, #RAND12_6c,
-                                RAND36_10 = PhysicalAndEmotional_Social4w) { #RAND12_7
+#' Generates unweighted RAND-12 PCS and MCS scores
+#'
+#' This function generates the unweighted RAND-12 PCS and MCS scores that range from 0 to 100, with higher scores indicating better HRQoL.
+#'
+#' @param data Data frame containing the variables needed for scoring RAND-12
+#' @param RAND36_1 The variable corresponding to RAND36_1 / RAND12_1
+#' @param RAND36_3b The variable corresponding to RAND36_3b / RAND12_2a
+#' @param RAND36_3d The variable corresponding to RAND36_3d / RAND12_2b
+#' @param RAND36_4b The variable corresponding to RAND36_4b / RAND12_3a
+#' @param RAND36_4c The variable corresponding to RAND36_4c / RAND12_3b
+#' @param RAND36_5b The variable corresponding to RAND36_5b / RAND12_4a
+#' @param RAND36_5c The variable corresponding to RAND36_5c / RAND12_4b
+#' @param RAND36_8 The variable corresponding to RAND36_8 / RAND12_5
+#' @param RAND36_9d The variable corresponding to RAND36_9d / RAND12_6a
+#' @param RAND36_9e The variable corresponding to RAND36_9e / RAND12_6b
+#' @param RAND36_9f The variable corresponding to RAND36_9f / RAND12_6c
+#' @param RAND36_10 The variable corresponding to RAND36_10 / RAND12_7
+#'
+#' @return A data frame with unweighted RAND-12 PCS and MCS scores
+#'
+#' @references
+#' Andersen, J. R., et al. (2022). Correlated physical and mental health composite scores for the RAND-36 and RAND-12 health surveys: Can we keep them simple? Health and Quality of Life Outcomes, 20(1), 89. https://doi.org/10.1186/s12955-022-01992-0
+#'
+#' @import tidyverse
+#' @export
+scoring_rand12_unweighted <- function(data,
+                                      RAND36_1 = Health_rating_now,
+                                      RAND36_3b = Moderate_activity4w,
+                                      RAND36_3d = Stairs4w,
+                                      RAND36_4b = Physical_health_lessthanwanted4w,
+                                      RAND36_4c = Physical_health_limited4w,
+                                      RAND36_5b = Psychological_health_lessthanwanted4w,
+                                      RAND36_5c = Psychological_healthlimitation4w,
+                                      RAND36_8 = Pain_limited_activities4w,
+                                      RAND36_9d = Relaxed_feeling4w,
+                                      RAND36_9e = Energetic_feeling4w,
+                                      RAND36_9f = Depressed_feeling4w,
+                                      RAND36_10 = PhysicalAndEmotional_Social4w) {
   library(tidyverse)
-
   # 1. Preserve raw data so they can be used for control
-  df <- df %>%
+  data <- data %>%
     mutate(
       GH1 = {{ RAND36_1 }},
       PF2 = {{ RAND36_3b }},
@@ -35,18 +52,18 @@ scoring_rand12_unweighted <- function(df,
       VT2 = {{ RAND36_9e }},
       MH4 = {{ RAND36_9f }},
       SF2 = {{ RAND36_10 }}
-  )
+    )
 
   # 2. Remove values that are outside possible alternatives
-  df <- df %>%
+  data <- data %>%
     mutate(across(c(RP2, RP3, RE2, RE3), ~case_when(. <= 2 ~ .)),
            across(c(PF2, PF4), ~case_when(. <= 3 ~ .)),
            across(c(GH1, BP2, SF2), ~case_when(. <= 5 ~ .)),
            across(c(MH3, VT2, MH4), ~case_when(. <= 6 ~ .)),
-                  )
+    )
 
   # 3. Recode to 0-100 score so that higher score means better health on all 12 items
-  df <- df %>%
+  data <- data %>%
     mutate(across(c(RP2, RP3, RE2, RE3), ~case_when(. == 1 ~ 0, . == 0 ~ 100)), #ok
            across(c(PF2, PF4), ~case_when(. == 2 ~ 0, . == 1 ~ 50, . == 0 ~ 100)), #ok
            across(c(GH1), ~case_when(. == 4 ~ 100, . == 3 ~ 75, . == 2 ~ 50, . == 1 ~ 25, . == 0 ~ 0)), #ok
@@ -56,21 +73,21 @@ scoring_rand12_unweighted <- function(df,
     )
 
   # 4. Create 8 domain scores (subscales)
-  df$PF <- rowMeans(df[,c("PF2","PF4")], na.rm = TRUE)
-  df$RP <- rowMeans(df[,c("RP2","RP3")], na.rm = TRUE)
-  df$BP <- df$BP2
-  df$GH <- df$GH1
-  df$VT <- df$VT2
-  df$SF <- df$SF2
-  df$RE <- rowMeans(df[,c("RE2","RE3")], na.rm = TRUE)
-  df$MH <- rowMeans(df[,c("MH3","MH4")], na.rm = TRUE)
+  data$PF <- rowMeans(data[,c("PF2","PF4")], na.rm = TRUE)
+  data$RP <- rowMeans(data[,c("RP2","RP3")], na.rm = TRUE)
+  data$BP <- data$BP2
+  data$GH <- data$GH1
+  data$VT <- data$VT2
+  data$SF <- data$SF2
+  data$RE <- rowMeans(data[,c("RE2","RE3")], na.rm = TRUE)
+  data$MH <- rowMeans(data[,c("MH3","MH4")], na.rm = TRUE)
 
   # 5. Create unweighted physical sum score (PCS12) and mental sum score (MCS12) (0-100 score)
-  df$PCS12_unwh <- rowMeans(df[,c("PF","RP","BP","GH")], na.rm = TRUE)
-  df$MCS12_unwh <- rowMeans(df[,c("VT","SF","RE","MH")], na.rm = TRUE)
+  data$PCS12_unwh <- rowMeans(data[,c("PF","RP","BP","GH")], na.rm = TRUE)
+  data$MCS12_unwh <- rowMeans(data[,c("VT","SF","RE","MH")], na.rm = TRUE)
 
   # 6. OBS! deletes all auxiliary variables. Omit this command if you want to see the whole calculation
-  df <- df[, !(names(df) %in% c("GH1","PF2","PF4","RP2","RP3","RE2","RE3","BP2","MH3","VT2","MH4","SF2"))]
+  data <- data[, !(names(data) %in% c("GH1","PF2","PF4","RP2","RP3","RE2","RE3","BP2","MH3","VT2","MH4","SF2"))]
 
-  return(df)
+  return(data)
 }
